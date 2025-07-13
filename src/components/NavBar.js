447ec0ba6c7e27498cd9d737cd006dac8f3efc31
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-07-13 16:50:45 +0300

    map method on arrays

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkItem = links.map((link, index) => {
+    return (
+      <a key={index} href={`#${link}`}>{link}</a>
+    );
+  })
+
+  return <nav>{/* display an <a> tag for each link here */}
+    {linkItem}
+  </nav>;
 }
 
 export default NavBar;
